6d0675bf6f03192ff111098b943e388ec5d5cfc7
othree <othree@gmail.com>
2014-06-11 17:29:47 +0800

    Add test

---
+++ test/test.js
@@ -0,0 +1,35 @@
+var should = require('should');
+require('mocha');
+
+
+var Router = require('../');
+
+var router = Router.new({
+  "/foo": "/foo",
+  "/foo/{id}": "/foo/[data]",
+  "/bar/{uid}/{post_id}": "/bar/[data]/[data]",
+});
+
+describe('node-r3', function () {
+  "use strict";
+
+  var dispatch;
+
+  it('string route', function () {
+    dispatch = router.match('/foo');
+    dispatch[0].should.equal('/foo');
+  });
+  it('unmatch', function () {
+    (router.match('/bar') === undefined).should.be.ok;
+    (router.match('/bar/xd') === undefined).should.be.ok;
+  });
+  it('capture', function () {
+    dispatch = router.match('/foo/12345');
+    dispatch[0].should.equal('/foo/[data]');
+
+    dispatch = router.match('/bar/othree/54321');
+    dispatch[0].should.equal('/bar/[data]/[data]');
+  });
+
+  // router.free();
+});
