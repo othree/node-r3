46e29f72bbf68097ed66fd151f9ef42d5a3ede05
othree <othree@gmail.com>
2014-06-12 22:31:20 +0800

    All test works

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+/*global describe: false, it: false, after: false */
 var should = require('should');
 require('mocha');
 
@@ -25,15 +26,19 @@ describe('node-r3', function () {
     dispatch[0].should.equal('/foo');
   });
   it('unmatch', function () {
-    (router.match('/bar') === undefined).should.be.ok;
-    (router.match('/bar/xd') === undefined).should.be.ok;
+    dispatch = (router.match('/bar') === undefined).should.be.ok;
+    dispatch = (router.match('/bar/xd') === undefined).should.be.ok;
   });
-  it('capture', function () {
-    // dispatch = router.match('/foo/12345');
-    // dispatch[0].should.equal('/foo/[data]');
 
-    // dispatch = router.match('/bar/othree/54321');
-    // dispatch[0].should.equal('/bar/[data]/[data]');
+  it('capture', function () {
+    dispatch = router.match('/foo/12345');
+    dispatch[0].should.equal('/foo/[data]');
+    dispatch[1][0].should.equal('12345');
+
+    dispatch = router.match('/bar/othree/54321');
+    dispatch[0].should.equal('/bar/[data]/[data]');
+    dispatch[1][0].should.equal('othree');
+    dispatch[1][1].should.equal('54321');
   });
 
   it('condition', function () {
